0ebc139b5d1dc1117c16af9b094e2e5a2898386b
Nazar Ch <nvc@nvc.com.ua>
2022-02-01 04:32:23 +0200

    Implement redirect component

--- src/common/Redirect.tsx
+++ src/common/Redirect.tsx
@@ -1,3 +1,8 @@
 import { FC } from 'react';
+import { useGenericStores } from './RootStore';
 
-export const Redirect: FC<{ to: string }> = () => <>NOT IMPLEMENTED</>;
+export const Redirect: FC<{ to: string }> = ({ to }) => {
+    const { router } = useGenericStores();
+    router.redirect(to);
+    return <></>;
+};
