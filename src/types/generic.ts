1fd64ee6d6e4eabbe21566eaee6a147902dbd547
Nazar Ch <nvc@nvc.com.ua>
2022-05-03 03:25:51 +0200

    Add utility types to make complex types look nice

--- src/types/generic.ts
+++ src/types/generic.ts
@@ -3,3 +3,15 @@ export type ExtractKeyType<T, K> = K extends keyof T ? T[K] : never;
 export type InstanceTypeWhenObject<T> = T extends new (...args: any) => any ? InstanceType<T> : T;
 
 export type Maybe<T> = T | null;
+
+// SOURCE: https://stackoverflow.com/questions/57683303/how-can-i-see-the-full-expanded-contract-of-a-typescript-type
+
+// expands object types one level deep
+export type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
+
+// expands object types recursively
+export type ExpandRecursively<T> = T extends object
+    ? T extends infer O
+        ? { [K in keyof O]: ExpandRecursively<O[K]> }
+        : never
+    : T;
