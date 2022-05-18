6424c29b706537c49dac35165d33be9ee9cda60f
Nazar Ch <nvc@nvc.com.ua>
2022-05-18 05:50:16 +0200

    Rename props for consistency

--- src/stores/Summon.ts
+++ src/stores/Summon.ts
@@ -1,4 +1,5 @@
 import { action, makeObservable, observable, runInAction } from 'mobx';
+import { ReadonlyDeep } from 'type-fest';
 
 // TODO: optimistic updates belongs here?
 export class Summon<T> {
@@ -6,13 +7,16 @@ export class Summon<T> {
         makeObservable(this);
     }
 
+    // Easy access to the original data type (externally exposed prop is read only)
+    __writableRawDataType: T = null as any;
+
     async fetch() {
         const data = await this.fetchApi();
         this.setData(data);
     }
 
     @action setData(data: T | null) {
-        this._rawData = data;
+        this._data = data;
     }
 
     async refresh() {
@@ -20,12 +24,18 @@ export class Summon<T> {
         await this.fetch();
     }
 
-    @observable protected _rawData: T | null = null;
+    @observable protected _data: T | null = null;
 
-    get rawData() {
-        if (!this._rawData) {
+    // Writable data for internal use
+    protected get data() {
+        if (!this._data) {
             throw new Error('Accessing non-loaded data');
         }
-        return this._rawData;
+        return this._data;
+    }
+
+    // Read only property for external use
+    get rawData() {
+        return this.data as ReadonlyDeep<T>;
     }
 }
